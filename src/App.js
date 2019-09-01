4a41c423b1cac96d673e6ba550616534b5bdbbd9
shinw97 <shinw97@gmail.com>
2019-09-02 01:16:49 +0800

    added scroll to anchor

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import Page2 from './Components/scroll-pages/Page2';
 import CreditBar from './Components/CreditBar';
 import Page3 from './Components/scroll-pages/Page3';
 import InfoBar from './Components/scroll-pages/InfoBar';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 function App() {
   return (
       <div>
         <NavBar/>
         <div className={'App'}>
+          <ScrollableAnchor id={'hello'}>
+            <div/>
+          </ScrollableAnchor>
           <Page1/>
+
+          <ScrollableAnchor id={'overview'}>
+            <div/>
+          </ScrollableAnchor>
           <Page2/>
+
+          <ScrollableAnchor id={'biography'}>
+            <div/>
+          </ScrollableAnchor>
           <Page3/>
           <InfoBar/>
           <Page1/>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,7 +10,7 @@ class NavBar extends React.Component {
   scrollBarProgress = () => {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (winScroll / height) * 100 + 5;
+    let scrolled = (winScroll / height) * 100 + 2;
     document.getElementById("progress").style.width = scrolled + "%";
   }
 
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
         <div className={'sticky-top'}>
           <nav
               className="navbar navbar-expand-lg bg-light navbar-light shadow">
-            <a className="navbar-brand" href="#"
+            <a className="navbar-brand" href="#hello"
                style={{'padding': '0px 30px'}}>A Webpage About Me</a>
             <button className="navbar-toggler" type="button"
                     data-toggle="collapse" data-target="#navbarNav"
@@ -31,10 +31,10 @@ class NavBar extends React.Component {
                  style={{'padding': '0px 50px'}}>
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link 1</a>
+                  <a className="nav-link" href="#overview">Overview</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link 2</a>
+                  <a className="nav-link" href="#biography">Biography</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Link 3</a>
@@ -47,7 +47,7 @@ class NavBar extends React.Component {
           </nav>
           <div className="progress" style={{'height': '5px'}}>
             <div className="progress-bar" role="progressbar" id={'progress'}
-                 aria-valuenow="75" aria-valuemin="0" aria-valuemax="95" style={{'width':'5%'}}/>
+                 aria-valuenow="75" aria-valuemin="0" aria-valuemax="95" style={{'width':'2%'}}/>
           </div>
         </div>
     );
